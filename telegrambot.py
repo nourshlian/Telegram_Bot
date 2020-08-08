ba2f80fea31e3a5427422990fd3054000e1555f3
Nour <nourshlian@gmail.com>
2020-08-09 02:45:17 +0300

    writing files and updates

--- telegrambot.py
+++ telegrambot.py
@@ -1,18 +1,27 @@
 from config import TELEGRAM_SEND_MESSAGE_URL
 import requests
 import time
-
+import json
 
 
 class TelegramBot:
 
     def __init__(self):
 
-        self._chat_id = None   # chat id for specific conversation
-        self._text = None      # incoming message
-        self._name = None      # the name of the user that had sent the message
-        self._chatids = []     # to save all incoming chatids
+        self._chat_id = None  # chat id for specific conversation
+        self._text = None  # incoming message
+        self._name = None  # the name of the user that had sent the message
+        self._history = {}  # a dictionary to save the data
+        self._chatids = [] # to save all chat ids that ever communicate this bot
+        self.init_ids()
 
+    def init_ids(self):
+        with open('chat_ids.txt', 'r') as file:
+            all = file.read()
+        print("in id init   --------- ", all.split(',')[:-1])
+        temp = all.split(',')[:-1]
+        for sid in temp:
+            self._chatids.append(int(sid))
 
     def parse_webhook_data(self, data):
 
@@ -29,15 +38,18 @@ class TelegramBot:
 
         self._chat_id = chatid
         if chatid not in self._chatids:
+            with open('chat_ids.txt', 'a') as f:
+                f.write(str(chatid) + ',')
             self._chatids.append(chatid)
 
-        if(in_msg == None):
+        if in_msg is None:
             self.incoming_message_text = ""
         else:
             self.incoming_message_text = in_msg.lower()
 
         self._name = name
-
+        key = message.get('message_id')
+        self._history[key] = message
 
     # what to replay to the message parsing specific messages and send the message to the telegram servers
     def replay(self):
@@ -57,28 +69,27 @@ class TelegramBot:
         else:
             self.outgoing_message_text = "I can't answer that, try:\nhello | how are you? | are you a telegram bot? | what is my chat id?"
 
-
         res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self._chat_id, self.outgoing_message_text))
 
-        return True if res.status_code == 200 else False
+        if_answerd = (res.status_code == 200)
+        if if_answerd:
+            self._history["answer"] = self.outgoing_message_text
 
-    # send the message to the telegram servers
-    def send_message(self):
+        with open("history.json", "a") as f:
+            json.dump(self._history, f)
 
-        res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(self._chat_id, self.outgoing_message_text))
+        return if_answerd
 
-        return True if res.status_code == 200 else False
+        # the auto send message, send a message every minute to all users that ever communicates the bot
 
-
-    # the auto send message, send a message every minute to all users that ever communicates the bot
     def auto_send(self):
 
-        while(1):
+        print(self._chatids)
+        while (1):
 
-            print (self._chatids)
-            for id in self._chatids:
-                requests.get(TELEGRAM_SEND_MESSAGE_URL.format(id, "this is the auto msg"))
-
-            time.sleep(60)
+            for chatid in self._chatids:
+                res = requests.get(TELEGRAM_SEND_MESSAGE_URL.format(chatid, "this is the auto msg"))
+                print(res.status_code)
+            time.sleep(30)
 
 
